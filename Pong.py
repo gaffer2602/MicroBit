016fea5bb0d0aa0a419555e4b9dd0aa2472121fb
gaffer2602 <99414025+gaffer2602@users.noreply.github.com>
2023-03-01 16:42:49 +0000

    Update and rename Pong to Pong.py

--- Pong.py
+++ Pong.py
@@ -4,20 +4,34 @@ def on_button_pressed_a():
         paddleB.change(LedSpriteProperty.X, -1)
 input.on_button_pressed(Button.A, on_button_pressed_a)
 
+def on_gesture_tilt_left():
+    game.resume()
+input.on_gesture(Gesture.TILT_LEFT, on_gesture_tilt_left)
+
+def on_button_pressed_ab():
+    game.pause()
+input.on_button_pressed(Button.AB, on_button_pressed_ab)
+
 def on_button_pressed_b():
     if paddleA.get(LedSpriteProperty.X) < 3:
         paddleA.change(LedSpriteProperty.X, 1)
         paddleB.change(LedSpriteProperty.X, 1)
 input.on_button_pressed(Button.B, on_button_pressed_b)
 
+def on_gesture_tilt_right():
+    game.game_over()
+input.on_gesture(Gesture.TILT_RIGHT, on_gesture_tilt_right)
+
 paddleB: game.LedSprite = None
 paddleA: game.LedSprite = None
+game.set_life(5)
 paddleA = game.create_sprite(2, 4)
 paddleB = game.create_sprite(3, 4)
 ball = game.create_sprite(randint(0, 4), 0)
 directionY = 1
 directionX = randint(-1, 1)
 basic.pause(500)
+game.set_score(0)
 sound = 1
 
 def on_forever():
@@ -25,15 +39,16 @@ def on_forever():
     ball.change(LedSpriteProperty.X, directionX)
     ball.change(LedSpriteProperty.Y, directionY)
     if ball.is_touching(paddleA) or ball.is_touching(paddleB):
+        game.add_score(1)
         ball.change(LedSpriteProperty.X, directionX * -1)
         ball.change(LedSpriteProperty.Y, -1)
         directionY = -1
         if sound == 0:
-            music.play_tone(262, music.beat(BeatFraction.HALF))
+            music.play_tone(294, music.beat(BeatFraction.HALF))
             basic.pause(100)
             sound = 1
         elif sound == 1:
-            music.play_tone(131, music.beat(BeatFraction.HALF))
+            music.play_tone(147, music.beat(BeatFraction.HALF))
             basic.pause(100)
             sound = 0
     else:
@@ -41,11 +56,14 @@ def on_forever():
             directionY = 1
             directionX = randint(-1, 1)
         elif ball.get(LedSpriteProperty.Y) >= 4:
+            music.play_tone(131, music.beat(BeatFraction.HALF))
+            directionY = -1
+            game.remove_life(1)
             ball.set(LedSpriteProperty.BLINK, 1)
             basic.pause(2000)
-            music.start_melody(music.built_in_melody(Melodies.DADADADUM),
-                MelodyOptions.ONCE)
-            game.game_over()
+            ball.set(LedSpriteProperty.BLINK, 0)
+            if game.is_game_over() == True:
+                music.play_melody("B4:1 - B4:1 - B4:1 - B4:5 G4:5 A4:5 B4:4 A4:1 B4:8", 180)
         if ball.get(LedSpriteProperty.X) <= 0:
             directionX = 1
         elif ball.get(LedSpriteProperty.X) >= 4:
