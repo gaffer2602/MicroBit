245aae2101f53a5557fe97a8f6e9608108f78595
gaffer2602 <99414025+gaffer2602@users.noreply.github.com>
2023-10-17 23:13:13 +0100

    Update Spanish_Inquisition.py

--- Spanish_Inquisition.py
+++ Spanish_Inquisition.py
@@ -1,13 +1,16 @@
+#Press button a
 def on_button_pressed_a():
+    #Define our x variable, which is what will loop radio group down from 256 to 0
     global x
-    for index in range(256):
-        radio.send_string("Nobody Expects The Spanish Inquisition")
-        if x > 0:
-            x += -1
-        elif x == 0:
+    x = 255
+    #Send that message to anyone on the radio channel
+    radio.send_string("Nobody Expects The Spanish Inquisition")
+    #Loop over 256 times (there's 256 radio groups)
+    for i in range(256):
+        radio.set_group(x)
+        #Iterate down by 1
+        x -= 1
+        #If x reaches the last radio group, bring it back to the start
+        if x == 0:
             x = 255
 input.on_button_pressed(Button.A, on_button_pressed_a)
-
-x = 0
-x = 255
-radio.set_group(x)
